033d5f5d3a991a96f01538a404b5a037cf57521f
imran <zunaeede@gmail.com>
2023-08-26 19:17:24 +0600

    main -make the calculator -using object -using function -using class -make it when using practicing object function

--- cal_using_object_function.php
+++ cal_using_object_function.php
@@ -6,11 +6,42 @@
     <title>Document</title>
 </head>
 <body>
-    
+<form action="prac6.php" method="get">
+        <input type="number" name="num1" placeholder="Enter 1st number"><br>
+        <input type="number" name="num2" placeholder="Enter 2nd number"><br>
+        <input type="symbol" name="symbol" placeholder="Enter any Symbil"><br>
+
+        <button type="submit" name="submit">Submit</button>
+    </form>
 </body>
 </html>
 
 <?php
 
+class po{
+    function panda($a, $b, $symbol){
+        if(isset($_REQUEST['submit'])){
+            if($symbol == '+'){
+                echo $a + $b;
+            }elseif($symbol == '-'){
+                echo $a - $b;
+            }elseif($symbol == '*'){
+                echo $a * $b;
+            }elseif($symbol == '/'){
+                echo $a / $b;
+            }else{
+                echo('Enter correct Symbol');
+            }
+        }
+    }
+}
+$calculator = new po();
+
+if(isset($_REQUEST['num1']) && isset($_REQUEST['num2']) && isset($_REQUEST['symbol'])){
+    $num1 = $_REQUEST['num1'];
+    $num2 = $_REQUEST['num2'];
+    $symbol = $_REQUEST['symbol'];
+    $calculator->panda($num1, $num2, $symbol);
+}
 
 ?>
